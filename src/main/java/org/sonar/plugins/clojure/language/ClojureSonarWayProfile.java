a6af992eef700dead58d9f8888aeaf1c25b6ff3b
Felipe Santiago <fsantiag@thoughtworks.com>
2018-04-15 21:44:27 +0800

    Add all Eastwood rules to Sonar-Clojure
    This needs to be improved. At the moment, the rules need to applied to
    the Sonar way profile in the class to take effect.
    It is necessary to create a profile config file that could be easily
    loaded inside the class.

--- src/main/java/org/sonar/plugins/clojure/language/ClojureSonarWayProfile.java
+++ src/main/java/org/sonar/plugins/clojure/language/ClojureSonarWayProfile.java
@@ -11,8 +11,29 @@ public final class ClojureSonarWayProfile implements BuiltInQualityProfilesDefin
         NewBuiltInQualityProfile profile = context.createBuiltInQualityProfile("Sonar way", ClojureLanguage.KEY);
         profile.setDefault(true);
 
-        NewBuiltInActiveRule rule1 = profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "unused-ret-vals");
-        rule1.overrideSeverity("BLOCKER");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "bad-arglists");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "constant-test");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "def-in-def");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "deprecations");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "keyword-typos");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "local-shadows-var");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "misplaced-docstrings");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "no-ns-form-found");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "non-clojure-file");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "redefd-vars");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "suspicious-expression");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "suspicious-test");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "unlimited-use");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "unused-fn-args");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "unused-locals");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "unused-meta-on-macro");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "unused-namespaces");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "unused-private-vars");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "unused-ret-vals");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "wrong-arity");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "wrong-ns-form");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "wrong-pre-post");
+        profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, "wrong-tag");
 
         profile.done();
     }
--- src/test/java/org/sonar/plugins/clojure/language/ClojureSonarWayProfileTest.java
+++ src/test/java/org/sonar/plugins/clojure/language/ClojureSonarWayProfileTest.java
@@ -4,10 +4,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class ClojureSonarWayProfileTest {
 
@@ -22,11 +25,35 @@ public class ClojureSonarWayProfileTest {
     }
 
     @Test
-    public void testIfProfileIsCreatedWithOnlyOneRule() {
+    public void testIfSonarwayProfileIsCreatedWithAllEastwoodRules() {
         BuiltInQualityProfilesDefinition.BuiltInQualityProfile profile = context.profile("clj", "Sonar way");
         List<BuiltInQualityProfilesDefinition.BuiltInActiveRule> rules = profile.rules();
-        assertThat(rules.size(), is(1));
-        assertThat(rules.get(0).ruleKey(), is("unused-ret-vals"));
+        assertThat(rules.size(), is(23));
+        List<String> ruleKeys = new ArrayList<>();
+        ruleKeys.addAll(asList("bad-arglists",
+                "constant-test",
+                "def-in-def",
+                "deprecations",
+                "keyword-typos",
+                "local-shadows-var",
+                "misplaced-docstrings",
+                "no-ns-form-found",
+                "non-clojure-file",
+                "redefd-vars",
+                "suspicious-expression",
+                "suspicious-test",
+                "unlimited-use",
+                "unused-fn-args",
+                "unused-locals",
+                "unused-meta-on-macro",
+                "unused-namespaces",
+                "unused-private-vars",
+                "unused-ret-vals",
+                "wrong-arity",
+                "wrong-ns-form",
+                "wrong-pre-post",
+                "wrong-tag"));
+        rules.stream().forEach(rule -> assertTrue(ruleKeys.contains(rule.ruleKey())));
     }
 
 }
