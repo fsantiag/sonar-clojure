8f92a0082b359837b3ddac0cb6a20fd5c71b18b2
Felipe Santiago <fsantiag@thoughtworks.com>
2018-04-15 19:36:54 +0800

    Remove unnecessary throws declaration

--- src/test/java/org/sonar/plugins/clojure/sensors/IssueTest.java
+++ src/test/java/org/sonar/plugins/clojure/sensors/IssueTest.java
@@ -11,7 +11,7 @@ public class IssueTest {
     private Issue issue;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         issue = new Issue(
                 "some-rule-id",
                 "This is a message!",
