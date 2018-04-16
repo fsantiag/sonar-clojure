6b6056f0b74bdc800ca7eee0565ef42bb28ffb5c
Felipe Santiago <fsantiag@thoughtworks.com>
2018-04-16 23:26:29 +0800

    Add tests for ClojureLanguage class

--- src/main/java/org/sonar/plugins/clojure/language/ClojureLanguage.java
+++ src/main/java/org/sonar/plugins/clojure/language/ClojureLanguage.java
@@ -1,12 +1,8 @@
 package org.sonar.plugins.clojure.language;
 
 import org.sonar.api.config.Configuration;
-import org.sonar.api.internal.apachecommons.lang.StringUtils;
 import org.sonar.api.resources.AbstractLanguage;
 import org.sonar.plugins.clojure.settings.ClojureProperties;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ClojureLanguage extends AbstractLanguage {
     public static final String KEY = "clj";
@@ -14,27 +10,18 @@ public class ClojureLanguage extends AbstractLanguage {
 
     private final Configuration config;
 
-    public ClojureLanguage(Configuration configuration) {
+    public ClojureLanguage(Configuration config) {
         super(KEY, NAME);
-        this.config = configuration;
+        this.config = config;
     }
 
     @Override
     public String[] getFileSuffixes() {
-        String[] suffixes = filterEmptyStrings(config.getStringArray(ClojureProperties.FILE_SUFFIXES_KEY));
+        String[] suffixes = config.getStringArray(ClojureProperties.FILE_SUFFIXES_KEY);
         if (suffixes.length == 0) {
-            suffixes = ClojureProperties.FILE_SUFFIXES_DEFAULT_VALUE.split(",");
+            suffixes = new String[]{ClojureProperties.FILE_SUFFIXES_DEFAULT_VALUE};
         }
         return suffixes;
     }
 
-    private String[] filterEmptyStrings(String[] stringArray) {
-        List<String> nonEmptyStrings = new ArrayList<>();
-        for (String string : stringArray) {
-            if (StringUtils.isNotBlank(string.trim())) {
-                nonEmptyStrings.add(string.trim());
-            }
-        }
-        return nonEmptyStrings.toArray(new String[nonEmptyStrings.size()]);
-    }
 }
---
+++ src/test/java/org/sonar/plugins/clojure/language/ClojureLanguageTest.java
@@ -0,0 +1,45 @@
+package org.sonar.plugins.clojure.language;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.api.config.internal.MapSettings;
+import org.sonar.plugins.clojure.settings.ClojureProperties;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ClojureLanguageTest {
+
+    private MapSettings settings;
+    private ClojureLanguage language;
+
+    @Before
+    public void setUp() throws Exception {
+        settings = new MapSettings();
+        language = new ClojureLanguage(settings.asConfig());
+    }
+
+    @Test
+    public void testClojureLanguageFileSuffixesWhenKeyIsDefault() {
+        assertThat(language.getFileSuffixes(), is(new String[]{".clj"}));
+    }
+
+    @Test
+    public void testClojureLanguageFileSuffixesWhenEmptyKeyIsSet() {
+        settings.setProperty(ClojureProperties.FILE_SUFFIXES_KEY, "");
+        assertThat(language.getFileSuffixes(), is(new String[]{".clj"}));
+    }
+
+    @Test
+    public void testClojureLanguageFileSuffixesWhenKeyIsCustom() {
+        settings.setProperty(ClojureProperties.FILE_SUFFIXES_KEY, ".foo");
+        assertThat(language.getFileSuffixes(), is(new String[]{".foo"}));
+    }
+
+    @Test
+    public void testClojureLanguageFileSuffixesWhenKeyIsMultiple() {
+        settings.setProperty(ClojureProperties.FILE_SUFFIXES_KEY, ".foo,.bar,.baz");
+        assertThat(language.getFileSuffixes(), is(new String[]{".foo", ".bar", ".baz"}));
+    }
+
+}
