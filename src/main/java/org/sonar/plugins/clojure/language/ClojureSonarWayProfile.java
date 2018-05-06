f6a3b16a08f4c55bd49bbc1ea62ee36b7b069de6
Felipe Santiago <fsantiag@thoughtworks.com>
2018-05-06 19:39:57 +0800

    Load sonar way profile using the getResourceAsStream to prevent failing during scan

--- src/main/java/org/sonar/plugins/clojure/language/ClojureSonarWayProfile.java
+++ src/main/java/org/sonar/plugins/clojure/language/ClojureSonarWayProfile.java
@@ -1,43 +1,34 @@
 package org.sonar.plugins.clojure.language;
 
 import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
-import org.sonar.api.utils.log.Logger;
-import org.sonar.api.utils.log.Loggers;
 import org.sonar.plugins.clojure.rules.ClojureLintRulesDefinition;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public final class ClojureSonarWayProfile implements BuiltInQualityProfilesDefinition {
 
-    public static final String CLOJURE_SONAR_WAY_PATH = "src/main/resources/clojure/sonar_way.json";
-    private static final Logger LOG = Loggers.get(ClojureSonarWayProfile.class);
+    public static final String CLOJURE_SONAR_WAY_PATH = "/clojure/sonar_way.json";
 
     @Override
     public void define(Context context) {
         NewBuiltInQualityProfile profile = context.createBuiltInQualityProfile("Sonar way", ClojureLanguage.KEY);
         profile.setDefault(true);
 
-        JsonProfile jsonProfile = new JsonProfile();
+        JsonProfile jsonProfile = readProfile();
 
-        try {
-            jsonProfile = readProfile();
-        } catch (FileNotFoundException e) {
-            LOG.error("Error while loading SonarWay profile file: ", e);
-        }
-
-        jsonProfile.getRuleKeys().stream()
+        jsonProfile.getRuleKeys()
                 .forEach(key -> profile.activateRule(ClojureLintRulesDefinition.REPOSITORY_KEY, key));
 
         profile.done();
     }
 
-    private JsonProfile readProfile() throws FileNotFoundException {
-        JsonReader reader = new JsonReader(new FileReader(CLOJURE_SONAR_WAY_PATH));
-        return new Gson().fromJson(reader, JsonProfile.class);
+    private JsonProfile readProfile() {
+        InputStream resourceAsStream = getClass().getResourceAsStream(CLOJURE_SONAR_WAY_PATH);
+        InputStreamReader inputStreamReader = new InputStreamReader(resourceAsStream);
+        return new Gson().fromJson(inputStreamReader, JsonProfile.class);
     }
 
 }
