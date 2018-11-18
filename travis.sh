762607df3dfd7d35cc93b1cb9b2a44912e3ace36
Felipe Santiago <fsantiag@thoughtworks.com>
2018-11-18 23:23:18 +0800

    Enabling sonarcloud addon for pull request analysis

--- travis.sh
+++ travis.sh
@@ -2,5 +2,4 @@ if [ ! -z "$TRAVIS_TAG" ]; then
     sed -i "s/[0-9].\+-SNAPSHOT/${TRAVIS_TAG:1}/g" pom.xml
 fi
 
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
-    -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=fsantiag -Dsonar.login=$SONAR_TOKEN
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package install sonar:sonar
