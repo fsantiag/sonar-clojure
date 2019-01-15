6ee49628c384b15c7eff4c327c2f13d111315d7c
Heikki Hämäläinen <heikki.hamalainen@solita.fi>
2019-01-15 10:18:35 +0200

    Add ancient-clj support for outdated dependencies
    Ancient clj is called by using Leiningen. If any outdated dependencies are found then they are marked as minor vulnerabilities in project.clj file. If for some reason dependency name as a string is not found in project.clj then vulnerability is added to first line of project.clj.

---
+++ src/test/java/org/sonar/plugins/clojure/sensors/ancient/AncientRulesTest.java
@@ -0,0 +1,37 @@
+package org.sonar.plugins.clojure.sensors.ancient;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
+import org.sonar.plugins.clojure.language.ClojureSonarWayProfile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertTrue;
+
+public class AncientRulesTest {
+
+    private BuiltInQualityProfilesDefinition.Context context;
+    private ClojureSonarWayProfile clojureSonarWayProfile;
+
+    @Before
+    public void setUp() {
+        context = new BuiltInQualityProfilesDefinition.Context();
+        clojureSonarWayProfile = new ClojureSonarWayProfile();
+        clojureSonarWayProfile.define(context);
+    }
+
+
+    @Test
+    public void testIfSonarwayProfileIsCreatedWithAllEastwoodRules() {
+        BuiltInQualityProfilesDefinition.BuiltInQualityProfile profile = context.profile("clj", "Sonar way");
+        List<BuiltInQualityProfilesDefinition.BuiltInActiveRule> rules = profile.rules();
+        //assertThat(rules.size(), is(23));
+        List<String> ruleKeys = new ArrayList<>();
+        ruleKeys.addAll(asList("ancient-dependency"));
+
+        ruleKeys.stream().forEach(eastwoodRule -> assertTrue(ruleKeys.contains(eastwoodRule)));
+    }
+}
