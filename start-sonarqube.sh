260422ff86a5da3f4d1e9578c7e090f4ddab8aac
Heikki Hämäläinen <heikki.hamalainen@solita.fi>
2019-01-16 20:48:33 +0200

    Simply remove existing container and start sonarqube

--- start-sonarqube.sh
+++ start-sonarqube.sh
@@ -2,12 +2,7 @@
 set -eu
 ./mvnw clean package
 docker build . --tag sonarqube_local_image
-if [ ! "$(docker ps -a -q -f 'name=sonarqube_local_test')" ]; then
-  echo "Sonar container is not running"
-  docker run --name sonarqube_local_test  -p 9000:9000 sonarqube_local_image
-else
-  echo "Sonar is running - removing and restarting the image"
-  docker rm -f sonarqube_local_test
-  docker run --name sonarqube_local_test  -p 9000:9000 sonarqube_local_image
-fi
+docker rm -f sonarqube_local_test
+docker run --name sonarqube_local_test  -p 9000:9000 sonarqube_local_image
+
 
