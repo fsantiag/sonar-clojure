d58e6281c00701f0adfca49e3027771467167aeb
Felipe Santiago <jfelipe.sfilho@gmail.com>
2020-04-18 10:33:13 -0300

    Update travis build command

--- travis.sh
+++ travis.sh
@@ -2,4 +2,4 @@ if [ ! -z "$TRAVIS_TAG" ]; then
     sed -i "s/[0-9].\+-SNAPSHOT/${TRAVIS_TAG:1}/g" pom.xml
 fi
 
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package install sonar:sonar
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package package org.jacoco:jacoco-maven-plugin:report sonar:sonar
